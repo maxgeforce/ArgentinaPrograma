00af5c8cd651df45d53d6f238ab571cdca02bb07
Maximiliano Fernandez <youremail@example.com>
2022-05-09 22:12:50 -0300

    First Commit

---
+++ src/app/app.js
@@ -0,0 +1,6 @@
+var message = 'Hello World';
+console.log(message);
+var message2 = 'Hola a a an';
+console.log(message2);
+var queOnda = 'Esta todo muy bravo';
+console.log(queOnda);
---
+++ src/app/app.ts
@@ -0,0 +1,8 @@
+let message : string = 'Hello World';
+console.log(message); 
+let message2 : string = 'Hola a a an';
+console.log(message2); 
+
+let queOnda : string = 'Esta todo muy bravo';
+console.log(queOnda);
+ 

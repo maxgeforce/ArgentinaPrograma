ad93540be0406db2a1369b0f69c0dbafdc05175d
Maximiliano Fernandez <youremail@example.com>
2022-05-26 20:11:31 -0300

    Rutas Configuradas en routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout/layout.component';
 
+// Definimos Nuestras Rutas
+
 const routes: Routes = [
+  { path: 'Header', component: HeaderComponent },
+  { path : 'Layout', component: LayoutComponent},
+  { path : 'Footer', component : FooterComponent},
 ]
+
+// Cargamos nuestras Rutas
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
